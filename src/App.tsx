0d03ce54a1996b570a84c33e4a8f12ecaa53aca7
sai reddy <sairamreddy2211@gmail.com>
2026-02-01 13:30:50 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router basename="/myprofile">
       <Routes>
-        {/* neww route for skill */}
+        {/* neww route for skills */}
         <Route path="/" element={<Landing />} />
       </Routes>
     </Router>
